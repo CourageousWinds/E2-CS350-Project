099cb3a8ed8dd39e673eef2a0fae9eed092e84c9
Carter <ccox027@odu.edu>
2022-11-03 21:38:00 -0400

    Omit1

---
+++ app/src/main/java/edu/odu/cs/cs350/pne/Omit.java
@@ -0,0 +1,10 @@
+
+
+public class Omit {
+
+    String hello = "hello";
+    String commonWords[] = {"is", "it", "the", "a", "be", "of", "to", "in", "have", "too", "and", "I", "that", "for", "you", "he", "his",
+                            ""}; 
+
+}
+
