aff280722b79b666d5682097f21e95e6a39aac76
cs_zelle001 <zelle001@odu.edu>
2022-11-06 11:15:34 -0500

    Worked on Document reader.

--- app/src/main/java/edu/odu/cs/cs350/pne/Document.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Document.java
@@ -2,8 +2,75 @@
  * Created 10/26 by cs_zelle001 for a starting point.
  */
 
+ //Shared package for project.
 package edu.odu.cs.cs350.pne;
 
+//Necessary Libraries.
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.util.*;
+
+
 public class Document {
+
+    public static void main(String[] args) throws IOException{
+
+        //Going to Read the words into a Map
+        Map<String, int> wordMap 
+            =hashMapFromFile(args);
+
+        //Iterate through Map
+        for (Map.Entry<String,int> entry :
+            wordMap.entrySet()){
+                system.out.println(entry.getKey() + " : "
+                 + entry.getValue())
+            }
+        
+    }
+
+    public static Map<String, int> hashMapFromFile(String filepath){
+        Map<String, int> mapText
+            = new HashMap<String, int>();
+            BufferedReader br = null;
+
+            try {
+                
+                //File object
+                File file = new File(filepath);
+
+                // create BufferedReader obj from the File
+                br = new BufferedReader(new FileReader(file));
+
+                String line = null;
+
+                //Read line by line
+                while ((line = br.readLine()) !=null) {
+                    String [] piece = line.split(":");
+
+                    //Assigning name/number
+                    String name = piece [0].trim();
+                    int number = piece [1].trim();
+
+                    //put name/number in hashmap if not empty
+                    if(!name.equals("") && !number.equals(""))
+                        map.put(name, number);
+                }
+        
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                //close bufferredReader
+                if (br!= null) {
+                    try {
+                        br.close();
+                    }
+                    catch (Exception e){
+                    };
+                }
+            }
+        return mapText;
+     }
     
 }
--- app/src/test/java/edu/odu/cs/cs350/pne/TestDocument.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestDocument.java
@@ -1,5 +1,53 @@
 package edu.odu.cs.cs350.pne;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+
 public class TestDocument {
+
+    @Test
+    public void main(String[] args) throws FileNotFoundException{
+        
+        Map<String, int> wordMap = new Map<String, int>();
+
+    }
+
+    @Test
+    public static Map<String, int>(){
+        Map<String, int> mapText
+            = new HashMap<String, int>();
+            BufferedReader br = null;
+            try {
+                
+                //File object
+                File file = new File(filepath);
+
+                // create BufferedReader obj from the File
+                br = new BufferedReader(new FileReader(file));
+                
+                String line = null;
+        
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                //close bufferredReader
+                if (br!= null) {
+                    try {
+                        br.close();
+                    }
+                    catch (Exception e){
+                    };
+                }
+            }
+        return mapText;
+     }
     
 }
+
+    
+
