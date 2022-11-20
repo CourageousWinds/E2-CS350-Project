62f7b11131fb8fd1b77d3fa422ff40d05a41d2a1
Joshua Miller <jmill035@odu.edu>
2022-11-20 02:13:07 -0500

    Modified Vocabulary.java

--- app/src/main/java/edu/odu/cs/cs350/pne/Vocabulary.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Vocabulary.java
@@ -3,10 +3,34 @@
  */
 
 package edu.odu.cs.cs350.pne;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.util.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Vocabulary {
+
+    public void main(String[] args) throws FileNotFoundException {
+
+        HashMap<String, Integer> vocab = new HashMap<String, Integer>();
     
+        Scanner txtFile = new Scanner(new File("/home/cs_jmill035/cs350Project/E2-CS350-Project/app/dataFiles/Test.txt"));
+        while (txtFile.hasNext()){
+            String word = txtFile.next();
+            if (vocab.containsKey(word)){
+                int count = vocab.get(word); 
+                vocab.put(word, count);
+            }
+            else{
+                vocab.put(word, 0);
+            }
+        }
+        txtFile.close();
+    
+        for (Map.Entry<String, Integer> entry: vocab.entrySet()){
+            System.out.println(entry);
+        }
+    }
 }
--- app/src/test/java/edu/odu/cs/cs350/pne/TestVocabulary.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestVocabulary.java
@@ -3,10 +3,10 @@ package edu.odu.cs.cs350.pne;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.Test;
-import org.apache.log4j.Logger;
+ 
 
 public class TestVocabulary {
     
